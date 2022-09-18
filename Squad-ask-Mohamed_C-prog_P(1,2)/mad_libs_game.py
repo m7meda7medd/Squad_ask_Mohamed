fbdf99f64175dc652e610d027ff7ba7b875fcdd5
mohamed <moahmed1907@gmail.com>
2022-09-18 23:53:29 +0200

    python part 1-2 tasks

---
+++ Squad-ask-Mohamed_C-prog_P(1,2)/mad_libs_game.py
@@ -0,0 +1,17 @@
+
+from tokenize import Name
+
+
+colour = str(input("enter a colour:"))
+Plr_noun= str(input("enter a plural noun:"))
+celebrity= str(input("enter the name of celebrity :"))
+
+print("Roses are",colour)
+print("%s are blue" %Plr_noun)
+print("I love {0} ".format(celebrity))
+
+
+
+
+
+
